5c234f125ccd6f0aea0bec1b4201b42acde4ee9a
Vienchau <chautrivien2103@gmail.com>
2026-07-05 17:32:23 +0000

    mit68240: update lecture 4

--- playgrounds/cloud-init/cloud-init.sh
+++ playgrounds/cloud-init/cloud-init.sh
@@ -65,3 +65,9 @@ virt-install --name ${VM_NAME} \
 
 
 virsh console ${VM_NAME}
+
+
+
+sudo nmcli connection modify enp1s0 ipv4.method manual \
+ipv4.addresses 192.168.1.100/24 ipv4.gateway 192.168.103.1 \
+ipv4.dns "8.8.8.8,8.8.4.4" connection.autoconnect yes
